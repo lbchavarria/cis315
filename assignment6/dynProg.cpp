181563d2f4caa267806110669ee4d5416f1cedb4
lbchavarria <lbchavarria@gmail.com>
2017-06-01 20:39:36 -0700

    still working on iterative

--- assignment6/dynProg.cpp
+++ assignment6/dynProg.cpp
@@ -8,7 +8,6 @@
 
 using namespace std;
 
-bool can_split;
 vector<string> dict;
 string s;
 int m;
@@ -89,7 +88,7 @@ bool botsplit(string x) {
     }
     return t[x.length()];
 }*/
-
+/*
 bool botsplit(string x) {
     if (x == "") {
         return false;
@@ -107,6 +106,51 @@ bool botsplit(string x) {
         }
     }
     return t[x.length()];
+}*/
+/*
+bool botsplit(string x) {
+    if (x.length() == 0) {
+        return false;
+    }
+    int index = 0;
+    int last = 0;
+    vector<bool> t(x.length()+1, false);
+    t[0] = true;
+    string word = "";
+    while (index < x.length()) {
+        word += x.substr(index, 1);
+        if (t[last] && dictionary1(word)) {
+            word += " ";
+            t[index] = true;
+            last = index;
+            index++;
+        }
+        else {
+            index++;
+        }
+    }
+    s = word;
+    return t[last];
+}*/
+
+bool botsplit(string x) {
+    if (x == "") {
+        return false;
+    }
+    vector<bool> t(x.length()+1);
+    t[0] = true;
+    for (int i = 0; i < x.length(); i++) {
+        for (int j = 1; j <= x.length()-i; j++) {
+            if (t[i] && dictionary1(x.substr(i,j))) {
+                t[i] = true;
+                s.append(x.substr(i, j));
+                s.append(" ");
+                //i += j;
+                break;
+            }
+        }
+    }
+    return t[0];
 }
 
 int main() {
@@ -125,7 +169,7 @@ int main() {
         istringstream iss(line);
         iss >> str;
         cout << "phrase number: " << i << endl;
-        cout << str << endl;
+        cout << str << endl << endl;
         cout << "iterative attempt:" << endl;
         //string bsplit = bsplit(str);
         if (botsplit(str)) {
